de288fcd3869cce13d595d1988692b3224ef4ae4
olloberganovyoqubjon <yoqubjonolloberganov@gmail.com>
2025-03-04 09:26:45 +0500

    telephone moduli

---
+++ telephome/src/main/java/uz/forall/telephome/TelephomeApplication.java
@@ -0,0 +1,13 @@
+package uz.forall.telephome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelephomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelephomeApplication.class, args);
+    }
+
+}
---
+++ telephome/src/test/java/uz/forall/telephome/TelephomeApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.forall.telephome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelephomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
