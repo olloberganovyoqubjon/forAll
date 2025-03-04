f8bb368435ee2896b7dd6f18f8f4151dfba2a2b6
olloberganovyoqubjon <yoqubjonolloberganov@gmail.com>
2025-03-04 09:41:19 +0500

    telephone moduli

---
+++ telephone/src/test/java/uz/forall/telephone/TelephoneApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.forall.telephone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelephoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
